ef4edc380287077b4770fa907a0c51b38d6a7692
Anthony BARRE <a@fasterize.com>
2020-02-21 15:55:33 +0100

    [bstats-server/59] Remove config variable

--- plugins/zzz_init_plugin.js
+++ plugins/zzz_init_plugin.js
@@ -1,15 +1,6 @@
 /*This plugin can be used as an immediate init plugin when you are loading
 boomerang asynchronously: */
 
-var config;
-if (BOOMR.window.boomr_beacon_url) {
-  config = {
-    beacon_url: BOOMR.window.boomr_beacon_url
-  };
-} else if (BOOMR.window.BOOMR_GLOBAL_CONFIG) {
-  config = BOOMR.window.BOOMR_GLOBAL_CONFIG;
-}
-
-if (config !== undefined) {
-  BOOMR.init(config);
+if (BOOMR.window.BOOMR_GLOBAL_CONFIG !== undefined) {
+  BOOMR.init(BOOMR.window.BOOMR_GLOBAL_CONFIG);
 }
